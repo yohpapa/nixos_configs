42c3ee07e5e19f22f0a05b7619d6bdb2d84e4c83
yohpapa <kemumaki.kemuo@gmail.com>
2026-01-11 16:55:26 +0900

    add go shell

---
+++ shells/go.nix
@@ -0,0 +1,39 @@
+# #                   _
+#   __ _  ___   _ __ (_)_  __
+#  / _` |/ _ \ | '_ \| \ \/ /
+# | (_| | (_) || | | | |>  <
+#  \__, |\___(_)_| |_|_/_/\_\
+#  |___/
+
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # Core
+    go
+    gopls
+
+    # Debugging & Linting
+    delve
+    golangci-lint
+
+    # Formatting & Tools
+    gotools # contains goimports
+    gofumpt
+
+    # Backend Development
+    air # live reload
+    sqldef # or go-migrate for DB migrations
+
+    # Shell tools
+    zsh
+    figlet
+  ];
+
+  shellHook = ''
+    export GOPATH=$HOME/go
+    export PATH=$GOPATH/bin:$PATH
+    figlet -w 200 This is a shell for Go-lang dev!
+    exec zsh
+  '';
+}
